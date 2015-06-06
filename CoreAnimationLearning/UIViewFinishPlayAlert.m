898f5210dee794e6a2a560977191ef6e1d932f86
chenjianfeng <chenjianfeng@youmi.net>
2015-06-06 21:10:42 +0800

    加入粒子效果

--- CoreAnimationLearning/UIViewFinishPlayAlert.m
+++ CoreAnimationLearning/UIViewFinishPlayAlert.m
@@ -24,6 +24,10 @@
     return self;
 }
 
+-(void)continueGame{
+    
+}
+
 -(void)showView{
     self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5];
     
@@ -60,6 +64,50 @@
     buttonNext.backgroundColor = [UIColor grayColor];
     [buttonBack addTarget:self action:@selector(buttonNextLevelPressed:) forControlEvents:UIControlEventTouchUpInside];
     [board addSubview:buttonNext];
+    
+    
+    //添加粒子效果,树叶往下掉
+    // 设置粒子发射的地方
+    CAEmitterLayer *snowEmitter = [CAEmitterLayer layer];
+    snowEmitter.emitterPosition = CGPointMake(self.bounds.size.width / 2.0, -50);
+    snowEmitter.emitterSize		= CGSizeMake(board.frame.size.width/2, 0);
+    
+    // Spawn points for the flakes are within on the outline of the line
+    snowEmitter.emitterMode		= kCAEmitterLayerSurface;
+    snowEmitter.emitterShape	= kCAEmitterLayerLine;
+    
+    // Configure the snowflake emitter cell
+    CAEmitterCell *snowflake = [CAEmitterCell emitterCell];
+    
+    snowflake.name = @"snowflake";
+    snowflake.birthRate		= 10.0;
+    snowflake.lifetime		= 2.0;
+    snowflake.lifetimeRange = 4;
+    
+    snowflake.scale = 0.05;
+    snowflake.velocity		= 200;				// 粒子速度
+    snowflake.velocityRange = 100;
+    snowflake.yAcceleration = 200;
+    snowflake.emissionLongitude = 0;
+    snowflake.emissionRange = 1 * M_PI;		// 周围发射角度
+    snowflake.spinRange		= 0.25 * M_PI;		// 粒子旋转角度
+    
+    snowflake.contents		= (id) [[UIImage imageNamed:@"leaf"] CGImage];
+    snowflake.color			= [[UIColor colorWithRed:0.600 green:0.658 blue:0.743 alpha:1.000] CGColor];
+    
+    // Make the flakes seem inset in the background
+    snowEmitter.shadowOpacity = 1.0;
+    snowEmitter.shadowRadius  = 0.0;
+    snowEmitter.shadowOffset  = CGSizeMake(0.0, 1.0);
+    snowEmitter.shadowColor   = [[UIColor whiteColor] CGColor];
+    
+    // Add everything to our backing layer below the UIContol defined in the storyboard
+    snowEmitter.emitterCells = [NSArray arrayWithObject:snowflake];
+    [self.layer insertSublayer:snowEmitter atIndex:0];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [snowEmitter setValue:[NSNumber numberWithFloat:0.5] forKeyPath:@"emitterCells.snowflake.birthRate"];
+    });
 }
 
 #pragma mark - 按钮事件
